e80318102dba0576b956cb0cb053c4b64003f6d5
Omar237p <s44450986@students.tu.edu.sa>
2024-05-08 14:00:55 +0300

    Omar fayz almotire 694 44450986

--- Test2-24S/Form1.cs
+++ Test2-24S/Form1.cs
@@ -6,5 +6,30 @@ namespace Test2_24S
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text) };
+
+
+            Array.Sort(numbers);
+            textBox1.Text = numbers[0].ToString();
+            textBox2.Text = numbers[1].ToString();
+            textBox3.Text = numbers[2].ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text) };
+
+           
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+         
+            textBox1.Text = numbers[0].ToString();
+            textBox2.Text = numbers[1].ToString();
+            textBox3.Text = numbers[2].ToString();
+        }
     }
 }
